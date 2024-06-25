647d2f6215ddde880aceb5882c4a7a0358adebee
Jilliane <110709632+jilliane-ruth@users.noreply.github.com>
2024-06-25 11:10:05 +0800

    .

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,14 +1,23 @@
-﻿namespace ProjectMonitoring.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace ProjectMonitoring.Models
 {
     public class Location
     {
         public int LocationId { get; set; }
+        public int ContractorId { get; set; }
+        [Required]
+        [DisplayName("Location Name")]
         public string LocationName { get; set; }
-        public string ContractorName { get; set; }
+        [DisplayName("Province")]
         public string Province { get; set; }
+        [DisplayName("Site Type")]
         public string SiteType { get; set; }
+        [DisplayName("Site Type")]
         public string Locality { get; set; }
+        [DisplayName("Locality")]
         public string SiteCoordniatorName { get; set; }
+        [DisplayName("Site Coordinator Name")]
         public string SiteCoordniatorNumber { get; set; }
 
         public Contractor Contractor { get; set; }
