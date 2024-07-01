51445e18729871433263d4286873edd73d9afcb1
Jilliane <110709632+jilliane-ruth@users.noreply.github.com>
2024-07-01 10:56:27 +0800

    .

--- Data/SupabaseService.cs
+++ Data/SupabaseService.cs
@@ -10,6 +10,59 @@ namespace ProjectMonitoring.Data
 {
     public class SupabaseService
     {
-        
+        private readonly HttpClient _httpClient;
+        private readonly string _functionUri;
+        private readonly string _anonKey;
+
+        public IConfiguration Configuration { get; }
+        public SupabaseService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var supabaseConfig = new SupabaseConfig
+            {
+                Url = Environment.GetEnvironmentVariable("SUPABASE_URL"),
+                AnonKey = Environment.GetEnvironmentVariable("SUPABASE_ANON_KEY")
+            };
+            services.AddSingleton(supabaseConfig);
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
     }
+}
+public class SupabaseConfig
+{
+    public string Url { get; set; }
+    public string AnonKey { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -9,14 +9,7 @@ builder.Services.AddControllersWithViews();
 
 var url = Environment.GetEnvironmentVariable("https://bacucuxysqlosdlmrqev.supabase.co");
 var key = Environment.GetEnvironmentVariable("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhY3VjdXh5c3Fsb3NkbG1ycWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzMzI4MTMsImV4cCI6MjAzMzkwODgxM30.SiJmWFLYmK7ovn8bgsLMWP-ehfS5ZvfXmRouuLGHEHk");
-
-/*var options = new Supabase.SupabaseOptions
-{
-    AutoConnectRealtime = true
-};
-
-var supabase = new Supabase.Client(url, key, options);*/
-//await supabase.InitializeAsync();
+// Validate the URL
 
 //supabase
 builder.Services.AddScoped<Supabase.Client>(_ => new Supabase.Client(builder.Configuration["SupabaseUrl"], builder.Configuration["SupabaseKey"], 
@@ -38,26 +31,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-var client = new HttpClient();
-var request = new HttpRequestMessage(HttpMethod.Post, url);
-request.Headers.Add("apikey", key);
-request.Headers.Add("Authorization", "Bearer " + key);
-var response = await client.SendAsync(request);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
